f6f5cc73ce4442e4a910a70224ce9acec435095b
Артём <zveronline@yandex.ru>
2023-05-24 08:02:01 +0300

    смена репозитория на ichbinkirgiz/sopds

--- configs/settings.py
+++ configs/settings.py
@@ -160,7 +160,10 @@ CACHE_MIDDLEWARE_KEY_PREFIX = "sopds"
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 STATIC_URL = '/static/'
-STATIC_ROOT = 'static'
+#STATIC_ROOT = 'static'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+]
 
 CONSTANCE_BACKEND = 'constance.backends.database.DatabaseBackend'
 
